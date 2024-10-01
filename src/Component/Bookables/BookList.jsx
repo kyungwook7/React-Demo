ee9a56e86098f832a6dc777b1b1597cc51763d6f
kyungwook7 <kyeongwook7@gmail.com>
2024-10-01 17:45:00 +0900

    #3 userState (상태값 관리) bookableIndex 값이 바뀔때 리액트가 UI를 다시 랜더링 하도록 한다. Next 버튼 이벤트도 상태값 변경 메서드를 싱행하도록 한다.

--- src/Component/Bookables/BookList.jsx
+++ src/Component/Bookables/BookList.jsx
@@ -1,25 +1,41 @@
-import { bookables } from "../../static.json"
+import {bookables} from "../../static.json"
+import {useState} from "react";
+import {FaArrowRight} from "react-icons/fa";
 
 function BookList() {
-    console.log("bookables", bookables);
-    const group = "kit";
-    let bookableIndex = 1;
+    const group = "Rooms";
+    // let bookableIndex = 1;
     const bookableGroup = bookables.filter(b => b.group === group);
+    // 상태값 관리를 해야할 변수 bookableIndex
+    // setBookableIndex 는 useState 가 리턴해주는 메서드. (값 변경 메서드)
+    const [bookableIndex, setBookableIndex] = useState(0)
 
-    function changeBookable(selectIndex) {
+/*      function changeBookable(selectIndex) {
         bookableIndex = selectIndex
+    } */
+
+    function nextBookableIndex() {
+        setBookableIndex((i) => (i+1) % bookableGroup.length)
+        // 상태값 변경 메서드의 인자 i는 bookableIndex 값
     }
 
     return (
+        <div>
         <ul className="items-list-nav">
             {bookableGroup.map((b, i) => (
                 <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-                    <button className="btn" onClick={() => changeBookable(i)}>
+                    <button className="btn" onClick={() => setBookableIndex(i)}>
                         {b.title}
                     </button>
                 </li>
             ))}
         </ul>
+            <p>
+                <button className="btn" onClick={nextBookableIndex}>
+                    <FaArrowRight/><span>Next</span>
+                </button>
+            </p>
+        </div>
     );
 }
 
