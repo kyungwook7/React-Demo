ee17a4acd34b3ba9add37088b92dda3c4b373d23
kyungwook7 <kyeongwook7@gmail.com>
2024-10-02 14:17:18 +0900

    #6 예약 날짜 UI 제어하기

--- src/Component/Bookings/BookingsPage.jsx
+++ src/Component/Bookings/BookingsPage.jsx
@@ -1,7 +1,15 @@
+import WeekPicker from "./WeekPicker.jsx";
+
 function BookingsPage() {
     return (
         <main className="Bookings-Page">
-            <h4>예약 가능 현황</h4>
+            <div>예약하기</div>
+            {/*<WeekPicker date="" day="" month=""/>
+                WeekPicker 에서 받을 값이 3개 -> 함수에서 props 변수 로 받거나
+                또는 오브젝트 분해하여 {date,day,month}
+            */}
+            {/* WeekPicker 에게 초기값으로 date 속성을 전달한다.*/}
+            <WeekPicker date={new Date()}/>
         </main>
     )
 }
